078365cee3f6b55b4f0edb44c7138fd30a596907
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-08-12 12:06:54 +0100

    GitHub Pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,7 +8,7 @@
 
 <p class="mb-6 text-center">
 	Want to check out all the routes on Project Cyan? Simply head over to the <a
-		href="https://de1.ocean-hosting.top:25003" class="underline">MTR system map</a
+		href="http://de1.ocean-hosting.top:25003" class="underline">MTR system map</a
 	>
 	or if you want a look at the world before you join, then head to
 	<a href="de1.ocean-hosting.top:25004">our Squaremap!</a> Want to start your own company to help
