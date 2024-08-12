c1f466a0948ec342d2ed705a2b20658fc1e11d8f
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-08-12 12:25:12 +0100

    More fixing

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,10 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'https://pufferfishtrans.awesomekalin.eu.org/404.html',
+			fallback: '404.html',
 		}),
 		paths: {
-			base: 'https://pufferfishtrans.awesomekalin.eu.org/',
+			base: 'pufferfishtrans.awesomekalin.eu.org',
 		},
 	},
 };
