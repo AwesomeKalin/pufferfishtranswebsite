5d0d5aab1e5840987914e96743a10785f47648eb
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-09-30 09:09:47 +0100

    Update real estate offerings

--- src/routes/real-estate/+page.svelte
+++ src/routes/real-estate/+page.svelte
@@ -7,12 +7,12 @@
 </h1>
 
 <h2 class="mb-4 text-center text-3xl font-bold">Currently avaliable for buy or rent:</h2>
-<h3 class="mb-3 text-2xl font-semibold">1-6 Atlantic Street:</h3>
+<h3 class="mb-3 text-2xl font-semibold">2-6 Atlantic Street:</h3>
 <ul class="mb-6 list-disc pl-6">
 	<li>Small, unfurnished house for 1</li>
 	<li>Surrounded by water</li>
 	<li>Nearest Bus Stop and Routes: Atlantis, No Routes Currently</li>
-	<li>Nearest Train Station and Routes: Airport, Blue Line from MangoRail</li>
+	<li>Nearest Train Station and Routes: Central City Hall, Silver Line from MangoRail</li>
 	<li>Buy or Rent: Best Offer</li>
 	<li>
 		Purchase by talking to one of our representatives on the <a
