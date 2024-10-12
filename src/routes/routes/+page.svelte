9af890853ac20dfe5787a9c593304031765a0462
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-10-12 16:53:39 +0100

    PT1 is now operational

--- src/routes/routes/+page.svelte
+++ src/routes/routes/+page.svelte
@@ -1,15 +1,14 @@
 <h2 class="mb-4 text-center text-3xl font-bold">Operational routes:</h2>
-<h3 class="mb-3 text-2xl font-semibold">None currently!</h3>
-
-<h2 class="mb-4 text-center text-3xl font-bold">Routes in Construction:</h2>
 <h3 class="mb-3 text-2xl font-semibold">PT1:</h3>
 <ul class="mb-6 list-disc pl-6">
     <li>Type: Bus</li>
     <li>From: Atlantis Racecourse</li>
     <li>To: Bayswater</li>
-    <li>Via: Halkound</li>
-    <li>Vehicle on Route: None currently!</li>
+    <li>Via: Avantra Pines</li>
+    <li>Vehicle on Route: Yutong U12</li>
 </ul><br />
+
+<h2 class="mb-4 text-center text-3xl font-bold">Routes in Construction:</h2>
 <h3 class="mb-3 text-2xl font-semibold">PT2:</h3>
 <ul class="mb-6 list-disc pl-6">
     <li>Type: Bus</li>
